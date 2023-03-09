3b7573dab1a6b7103b937f8f63e756e7e64c7de3
Madis99 <sankamah40@gmail.com>
2023-03-09 17:17:59 +0000

    Added list & dict

---
+++ main.py
@@ -0,0 +1,18 @@
+joels_info={"name":"Joel",
+"age": 13,
+"hobbies":{"outdoor":["roaming","ampe"],"indoor":["ludu","teletabies"]}}
+hobbies=joels_info["hobbies"]
+print[hobbies]
+outdoor=hobbies["outdoor"]
+print[outdoor]
+second_outdoor_hobby=outdoor[1]
+print[second_outdoor_hobby]
+students_grade={}
+studentsgrade={"Mike":"A",
+"Aaron":"B",
+"Samuel":"C"}
+students_grade=studentsgrade
+print[students_grade]
+students_grade.pop["Aaron"]
+age=input("what is your age?\n")
+print (f "your age is {15}")
